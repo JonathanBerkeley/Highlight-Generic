b5d4e26669be9391bab80ea50febc3e6fc412435
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2022-02-15 13:11:34 +0000

    Minor change

--- Highlight/constants.h
+++ Highlight/constants.h
@@ -2,7 +2,7 @@
 #include "pch.h"
 
 namespace constants {
-    const LPCWSTR DLL_NAME = L"Generic";
-    const std::string VERSION = "0.0.4";
-    const std::wstring HIJACK_TEXT = L"Process hijacked";
+    const LPCWSTR DLL_NAME { L"Generic" };
+    const std::string VERSION { "0.0.5" };
+    const std::wstring HIJACK_TEXT { L"Process hijacked" };
 }
