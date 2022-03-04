1c03460b1ba7eda43cf3ff09c3ac0c0b6a81fa09
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2022-03-04 23:58:08 +0000

    Update constants

--- Highlight/constants.h
+++ Highlight/constants.h
@@ -4,6 +4,6 @@
 namespace constants {
     const LPCWSTR DLL_NAME { L"Generic" };
     const std::string VERSION { "0.0.7" };
-    const std::wstring HIJACK_TEXT { L"A-crazy-new-world" };
+    const std::wstring HIJACK_TEXT { L"Process hijacked" };
     static constexpr bool DBG = false;
 }
